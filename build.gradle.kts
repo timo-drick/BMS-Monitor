2c499decc9e74d91fae15374e45e254efd639c3f
Timo Drick <t.drick@apps-on-air.de>
2024-04-07 10:36:53 +0200

    Migrated plugins to use toml file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,10 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version libs.versions.agp apply false
-    id("org.jetbrains.kotlin.android") version libs.versions.kotlin apply false
-    id("com.google.devtools.ksp") version libs.versions.ksp apply false
-    kotlin("plugin.serialization") version libs.versions.kotlin apply false
-    id("com.github.ben-manes.versions") version libs.versions.benManesPlugin
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.kotlin.serialization) apply false
+    alias(libs.plugins.benmanes)
 }
 
 fun isNonStable(version: String): Boolean {
